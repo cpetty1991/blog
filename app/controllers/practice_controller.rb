6f53b5883abd5e66ec015b595df0472c761c42cd
Christopher Petty <cpetty1991@yahoo.com>
2017-09-18 12:32:54 -0400

    first commit

---
+++ app/controllers/practice_controller.rb
@@ -0,0 +1,14 @@
+class PracticeController < ApplicationController
+  def index
+
+  @name = params[:name]
+
+  end
+
+  def about
+  	
+  	@color = params[:color]
+  	@user = params[:user]
+  	
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :comments
+  resources :posts
+  root 'posts#index'
+
+  get 'about' => 'practice#about'
+
+  get 'index' => 'practice#index'
+
+  post 'index' => 'practice#index'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
